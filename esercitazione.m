f41ddd9aa3729fb4e590f88c09d82982367403a5
ema00chiaro <84520939+ema00chiaro@users.noreply.github.com>
2022-03-03 16:44:39 +0100

    aggiunte MIALU e MIALDL

--- esercitazione.m
+++ esercitazione.m
@@ -232,7 +232,78 @@ function testRadice()
     disp("errori totali: " + err)
 end
 
+function x = mialu(A,b)
+% x = mialu(A,b)
+% La funzione calcola la soluzione del sistema lineare Ax=b 
+% mediante il metodo della fattorizzazione LU con pivoting parziale
+% Input:
+%     A - la matrice del sistema (n*n)
+%     b - il vettore dei termini noti (n)
+% Output:
+%     x - la soluzione trovata
+    [row,col]= size(A);
+    if row ~= col, error("Matrice non quadrata, errore!"); end
+    n = row;
+    [row,col]= size(b);
+    if col ~= 1 || row ~= n, error("Il vettore dei termini noti non ha " + ...
+                            "dimensioni adeguate, errore!"); end
+    for i = 1:n-1
+        if A(i,i) == 0, error("Matrice singolare");end
+        A(i+1:n, i) = A(i+1:n, i)/A(i,i); % Gauss
+        A(i+1:n,i+1:n) = A(i+1:n,i+1:n) - A(i+1:n, i)*A(i, i+1:n);
+    end
+    
+    x = b;
+    %Ly=b
+    for i = 2:n
+        x(i:n) = x(i:n) - A(i:n,i-1)*x(i-1);
+    end
+    %Ux=y
+    for i = n:-1:1
+        x(i) = x(i)/A(i,i);
+        x(1:i-1) = x(1:i-1) - A(1:i-1,i)*x(i);
+    end
+    return
+end
 
+function x = mialdl(A,b)
+% x = mialu(A,b)
+% La funzione calcola la soluzione del sistema lineare Ax=b 
+% mediante il metodo della fattorizzazione LDL'
+% Input:
+%     A - la matrice del sistema (n*n)
+%     b - il vettore dei termini noti (n)
+% Output:
+%     x - la soluzione trovata
+    [row,col]= size(A);
+    if row ~= col, error("Matrice non quadrata, errore!"); end
+    n = row;
+    [row,col]= size(b);
+    if col ~= 1 || row ~= n, error("Il vettore dei termini noti non ha " + ...
+                            "dimensioni adeguate, errore!"); end
+    
+    if A(1,1) <= 0, error("Matrice non sdp");end
+    A(2:n,1) = A(2:n,1)/A(1,1);
+    for j = 2:n
+        %FIXME da rivedere questa cosa qua
+        v = diag(diag(A(1:j-1,1:j-1)))*A(j,1:j-1)';
+        A(j,j) = A(j,j) - A(j,1:j-1)*v;
+        if a(j,j) <= 0, error("non ha diagonale positiva");end
+        A(j+1:n,j) = (A(j+1:n,j) - A(j+1:n,1:j-1)*v)/A(j,j);
+    end
+    
+    x = b;
+    %Ly2=b
+    for i = 2:n
+        x(i:n) = x(i:n) - A(i:n,i-1)*x(i-1);
+    end
+    %Dy1=y2
+    x = x./diag(A);
+    %L'x=y1
+    for i=n-1:-1:1
+        x(1:i) = x(1:i)-A(i+1,1:i)'*x(i+1);
+    end
+end
 
 
 
