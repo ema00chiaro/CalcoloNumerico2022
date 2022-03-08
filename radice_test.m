f710f6a2cb3c8457b16facf7989bcef5f40aff8f
ema00chiaro <84520939+ema00chiaro@users.noreply.github.com>
2022-03-08 14:17:38 +0100

    radice COMPLETA (si spera)

--- radice_test.m
+++ radice_test.m
@@ -1,21 +1,23 @@
-% testRadice();
-disp(radice2(10));
+testRadice();
+% disp(radice(-2));
 
 function x = radice(x)
 % x = radice(x)
 % 
-% la funziona calcola la radice di un numero mediante le operazioni elementari
+% la funziona calcola la radice di un numero 
+% mediante le operazioni elementari
 % INPUT:
 %     x - il numero di cui si vuole trovare la radice
 % OUTPUT:
 %     x - la radice del numero
     if x < 0
-        error("errore: numero negativo, impossibile calcolare la radice!"); 
+        error("errore: numero negativo, " + ...
+            "impossibile calcolare la radice!"); 
     end
     if (x == 0 || x == 1), return, end
     
     sq_old = x;
-    sq = (1/2)*(sq_old+(x/sq_old)); % x/2 + 1/2
+    sq = x/2 + 1/2; % prima iterazione
     while ( abs(sq - sq_old) > eps)
         sq_old = sq;
         sq = (1/2)*(sq_old+(x/sq_old));
@@ -23,30 +25,6 @@ function x = radice(x)
     x = sq;
     return
 end
-
-function x = radice2(x)
-% x = radice(x)
-% 
-% la funziona calcola la radice di un numero mediante le operazioni elementari
-% INPUT:
-%     x - il numero di cui si vuole trovare la radice
-% OUTPUT:
-%     x - la radice del numero
-    if x < 0
-        error("errore: numero negativo, impossibile calcolare la radice!"); 
-    end
-    if (x == 0 || x == 1), return, end
-    
-    sq = x;
-    while ( sq*sq ~= x)
-        sq_old = sq;
-        sq = (1/2)*(sq+(x/sq));
-        if abs(sq_old - sq) == 0, break; end
-    end
-    x = sq;
-    return
-end
-
 
 function testRadice()
     inf = 1e-10;
@@ -58,15 +36,19 @@ function testRadice()
         i = i+1;
         mia = radice(x);
         vera = sqrt(x);
-        if (abs(vera - mia) > eps)
+%         disp("vera: "+ vera);
+%         disp("mia: "+ mia);
+%         disp(abs((vera-mia)/vera));
+        if (abs((vera-mia)/vera) > eps)
             disp("ITERAZIONE " + i+ " numero: " + x);
             err = err + 1; 
-            disp("vera: "+ vera);
-            disp("mia: "+ mia);
-            disp ("diffe: " + abs(vera - mia));
+            disp("vera: "+ vera + "diff num rad: " + abs(x-vera^2));
+            disp("mia: "+ mia + "diff num rad: " + abs(x-mia^2));
+            disp ("diffe: " + abs(mia^2 - vera^2));
             disp("QUI QUALCOSA NON VA!");
         end
         x = x*10;
     end
+    disp("i = " + i);
     disp("errori totali: " + err)
 end
