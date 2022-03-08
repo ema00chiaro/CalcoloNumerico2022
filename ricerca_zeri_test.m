05aa721469700e0e6ba0503d1267a3cf53b2e9c8
ema00chiaro <84520939+ema00chiaro@users.noreply.github.com>
2022-03-08 15:03:09 +0100

    aggiustatina funzione per ricerca zeri

--- ricerca_zeri_test.m
+++ ricerca_zeri_test.m
@@ -1,45 +1,45 @@
-% f = @functionToPass;
-% f1 = @functionToPassDerivata;
-% disp("newton");
-% [x,i] = newton(f,f1,1,10^-3);
-% disp(x);
-% disp(i);
-% [x,i] = newton(f,f1,1,10^-6);
-% disp(x);
-% disp(i);
-% [x,i] = newton(f,f1,1,10^-9);
-% disp(x);
-% disp(i);
-% [x,i] = newton(f,f1,1,10^-12);
-% disp(x);
-% disp(i);
-% disp("secanti");
-% [x,i] = secanti(f,1,0.99,10^-3);
-% disp(x);
-% disp(i);
-% [x,i] = secanti(f,1,0.99,10^-6);
-% disp(x);
-% disp(i);
-% [x,i] = secanti(f,1,0.99,10^-9);
-% disp(x);
-% disp(i);
-% [x,i] = secanti(f,1,0.99,10^-12);
-% disp(x);
-% disp(i);
-% disp("steffensen");
-% [x,i] = steffensen(f,1,10^-3);
-% disp(x);
-% disp(i);
-% [x,i] = steffensen(f,1,10^-6);
-% disp(x);
-% disp(i);
-% [x,i] = steffensen(f,1,10^-9);
-% disp(x);
-% disp(i);
-% [x,i] = steffensen(f,1,10^-12);
-% disp(x);
-% disp(i);
-% 
+f = @functionToPass;
+f1 = @functionToPassDerivata;
+disp("newton");
+[x,i] = newton(f,f1,1,10^-3);
+disp(x);
+disp(i);
+[x,i] = newton(f,f1,1,10^-6);
+disp(x);
+disp(i);
+[x,i] = newton(f,f1,1,10^-9);
+disp(x);
+disp(i);
+[x,i] = newton(f,f1,1,10^-12);
+disp(x);
+disp(i);
+disp("secanti");
+[x,i] = secanti(f,1,0.99,10^-3);
+disp(x);
+disp(i);
+[x,i] = secanti(f,1,0.99,10^-6);
+disp(x);
+disp(i);
+[x,i] = secanti(f,1,0.99,10^-9);
+disp(x);
+disp(i);
+[x,i] = secanti(f,1,0.99,10^-12);
+disp(x);
+disp(i);
+disp("steffensen");
+[x,i] = steffensen(f,1,10^-3);
+disp(x);
+disp(i);
+[x,i] = steffensen(f,1,10^-6);
+disp(x);
+disp(i);
+[x,i] = steffensen(f,1,10^-9);
+disp(x);
+disp(i);
+[x,i] = steffensen(f,1,10^-12);
+disp(x);
+disp(i);
+
 f = @functionToPass2;
 f1 = @functionToPass2Derivata;
 disp("newton2");
@@ -69,16 +69,16 @@ disp(i);
 disp(x);
 disp(i);
 disp("steffensen2");
-[x,i] = steffensen(f,1,10^-3,100000);
+[x,i] = steffensen(f,1,10^-3);
 disp(x);
 disp(i);
-[x,i] = steffensen(f,1,10^-6,100000);
+[x,i] = steffensen(f,1,10^-6);
 disp(x);
 disp(i);
-[x,i] = steffensen(f,1,10^-9,100000);
+[x,i] = steffensen(f,1,10^-9);
 disp(x);
 disp(i);
-[x,i] = steffensen(f,1,10^-12,100000);
+[x,i] = steffensen(f,1,10^-12);
 disp(x);
 disp(i);
 
@@ -104,16 +104,16 @@ function [x,i] = newton(f,f1,x0,tol,maxiter)
 % la seguente funzione implementa il metodo di newton per 
 % ricercare gli zeri di una funzione
 % INPUT:
-%     f - la funzione di cui si vogliono conoscere uno zero
+%     f - la funzione di cui si vuole conoscere una radice
 %     f1 - la derivata prima della funzione f
-%     x0 - il punto di partenza per la ricerca dello zero
+%     x0 - il punto di innesco
 %     tol - la tolleranza del metodo
 %     maxiter - il numero massimo di iterazioni voluto (default 200)
 % OUTPUT:
 %     x - la soluzione trovata
 %     i - numero di iterazioni effettuate
     if nargin == 4, maxiter = 200; end
-    if maxiter < 200, maxiter = 200; end
+    if feval(f,x0) == 0, i = 1; x = x0; return, end
     x_old = x0;
     for i = 0:maxiter
         x = x_old-feval(f,x_old)/feval(f1,x_old);
@@ -130,8 +130,8 @@ function [x,i] = secanti(f,x0,x1,tol,maxiter)
 % la funzione implementa il metdo delle secanti
 % per la ricerca degli zeri di una funzione
 % INPUT:
-%     f - la funzione di cui si vogliono conoscere uno zero
-%     x0 - il punto di partenza per la ricerca dello zero
+%     f - la funzione di cui si vuole conoscere una radice
+%     x0 - il punto di innesco
 %     x1 - il punto dopo la prima iterazione
 %     tol - la tolleranza del metodo
 %     maxiter - il numero massimo di iterazioni voluto (default 200)
@@ -139,20 +139,21 @@ function [x,i] = secanti(f,x0,x1,tol,maxiter)
 %     x - la soluzione trovata
 %     i - numero di iterazioni effettuate
     if nargin == 4, maxiter = 200; end
-    if maxiter < 200, maxiter = 200; end
+    if feval(f,x0) == 0, i = 1; x = x0; return, end
+    if feval(f,x1) == 0, i = 1; x = x1; return, end
     x_oldest = x0;
+    fx_oldest = feval(f,x_oldest);
     x_old = x1;
     for i = 0:maxiter
         fx_old = feval(f,x_old);
-        x = x_old - ((x_old-x_oldest)*fx_old)/(fx_old-feval(f,x_oldest));
-        if abs(x-x_old) <= tol*(1+abs(x_old)), break; end
-        appo = x_old;
+        x = x_old - ((x_old-x_oldest)*fx_old)/(fx_old-fx_oldest);
+        if abs(x-x_old) <= tol*(1+abs(x_old)), return, end
+        x_oldest = x_old;
         x_old = x;
-        x_oldest = appo;
+        fx_oldest = fx_old;
     end
-    if(i == maxiter), print("Soluzione non trovata. " + ...
+    if(i == maxiter), error("Soluzione non trovata. " + ...
             "Numero massimo di iterazioni (" + i +") raggiunto!"); end
-    return
 end
 
 function [x,i] = steffensen(f,x0,tol,maxiter)
@@ -161,25 +162,24 @@ function [x,i] = steffensen(f,x0,tol,maxiter)
 % la funzione implementa il metdo di Steffensen
 % per la ricerca degli zeri di una funzione
 % INPUT:
-%     f - la funzione di cui si vogliono conoscere uno zero
-%     x0 - il punto di partenza per la ricerca dello zero
+%     f - la funzione di cui si vuole conoscere una radice
+%     x0 - il punto di innesco
 %     tol - la tolleranza del metodo
 %     maxiter - il numero massimo di iterazioni voluto (default 200)
 % OUTPUT:
 %     x - la soluzione trovata
 %     i - numero di iterazioni effettuate
     if nargin == 3, maxiter = 200; end
-    if maxiter < 200, maxiter = 200; end
+    if feval(f,x0) == 0, i = 1; x = x0; return, end
     x_old = x0;
     for i = 0:maxiter
         fx_old = feval(f,x_old);
         diff = feval(f,x_old+fx_old) - fx_old;
         if diff == 0, error("Errore: Divisone per zero! "); end
-        x = x_old-(fx_old^2)/(diff);
-        if abs(x-x_old) <= tol*(1+abs(x_old)), return; end
+        x = x_old-(fx_old^2)/diff;
+        if abs(x-x_old) <= tol*(1+abs(x_old)), return, end
         x_old = x;
     end
     if(i == maxiter), error("Soluzione non trovata. " + ...
             "Numero massimo di iterazioni (" + i +") raggiunto!"); end
-    return
 end
