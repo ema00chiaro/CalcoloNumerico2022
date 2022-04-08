380a3bf7b41ef61441760ee7933676248b25b90c
ema00chiaro <84520939+ema00chiaro@users.noreply.github.com>
2022-04-08 16:55:14 +0200

    funzione test per il prof

--- radice_test.m
+++ radice_test.m
@@ -1,4 +1,6 @@
-testRadice();
+[x,er] = testRadice(-10,10,20);
+disp(x)
+disp(er)
 % disp(radice(-2));
 % x=logspace(-10,10,20);
 % for i = 1:20
@@ -32,25 +34,54 @@ function x = radice(x)
     return
 end
 
-function testRadice()
-    x=logspace(-10,10,20);
-    err = 0;
-    for i = 1:20
-        mia = radice(x(i));
-        vera = sqrt(x(i));
-        errAss = abs((vera-mia));
-        errRel = errAss/vera;
-        disp("ITERAZIONE " + i);
-        disp("num: " + x(i));
-%         disp ("err ass: " + errAss);
-        disp ("err rel: " + errRel);
-        if (errRel > eps)
-            err = err + 1; 
-            disp("vera: "+ vera + " diff num rad: " + abs(x(i)-vera^2));
-            disp("mia: "+ mia + " diff num rad: " + abs(x(i)-mia^2));
-            disp("QUI QUALCOSA NON VA!");
-        end
+% function testRadice()
+%     x=logspace(-10,10,20);
+%     err = 0;
+%     for i = 1:20
+%         mia = radice(x(i));
+%         vera = sqrt(x(i));
+%         errAss = abs((vera-mia));
+%         errRel = errAss/vera;
+% %         disp("ITERAZIONE " + i);
+% %         disp("num: " + x(i));
+% %         disp ("err ass: " + errAss);
+% %         disp ("err rel: " + errRel);
+%         disp(x(i) +","+errRel)
+%         if (errRel > eps)
+%             err = err + 1; 
+%             disp("vera: "+ vera + " diff num rad: " + abs(x(i)-vera^2));
+%             disp("mia: "+ mia + " diff num rad: " + abs(x(i)-mia^2));
+%             disp("QUI QUALCOSA NON VA!");
+%         end
+%     end
+%     disp("i = " + i);
+%     disp("errori totali: " + err)
+% end
+
+
+function [x,er] = testRadice(a,b,n)
+% [x,er] = testRadice(a,b,n)
+% 
+% Funzione per testare la funzione "x = radice(x)" su n valori 
+% equispaziati logaritmicamente in un certo intervallo [10^a,10^b].
+% 
+% INPUT:
+%     a - primo esponente intervallo
+%     b - secondo esponente intervallo
+%     n - numero di valori equispaziati
+%         logaritmicamente voluti
+% 
+% OUTPUT:
+%     x - vettore contenente gli n valori 
+%         equispaziati logaritmicamente
+%     er - vettore contenente i vari errori 
+%          relativi rispetto alla funzione 
+%          "x = sqrt(x)" proposta da matlab
+
+    x = logspace(a,b,n)';
+    er = x;
+    for i = 1:n
+        er(i) = abs(radice(x(i))-sqrt((x(i))))/sqrt(x(i));
     end
-    disp("i = " + i);
-    disp("errori totali: " + err)
+    return
 end
