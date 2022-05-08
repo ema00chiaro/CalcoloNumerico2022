7fb48993d93aa8cf4705af5e65c0682168378703
ema00chiaro <84520939+ema00chiaro@users.noreply.github.com>
2022-05-08 21:27:26 +0200

    fix

--- Es19.m
+++ Es19.m
@@ -1,5 +1,5 @@
-disp(Chebyshev(2,1,2))
-function xc = Chebyshev(n,a,b)
+disp(Chebyshev(1,2,2))
+function xc = Chebyshev(a,b,n)
     teta = (n:-1:0);
     teta = (teta.*2 + 1).*(pi/(2*(n+1)));
     xc = cos(teta).*((b-a)/2) + ((a+b)/2);
