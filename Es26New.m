5b166ca260e8b13411657904d0d9543b7a657079
ema00chiaro <84520939+ema00chiaro@users.noreply.github.com>
2022-06-13 15:04:25 +0200

    sdf

--- Es26New.m
+++ Es26New.m
@@ -1,35 +1,54 @@
-composita(@functionToPass,0,1,7,10^-6);
+tols = [10^-2;10^-3;10^-4;10^-5;10^-6];
+ris = zeros(1,5);
+vals = zeros(1,5);
+errs = zeros(1,5);
+for i = 1:9
+    if i == 8
+    else
+        disp("ITERAZIONE-------- " + i);
+        [ris(1),errs(1),vals(1)] = composita(@functionToPass,0,1,i,10^-2);
+        [ris(2),errs(2),vals(2)] = composita(@functionToPass,0,1,i,10^-3);
+        [ris(3),errs(3),vals(3)] = composita(@functionToPass,0,1,i,10^-4);
+        [ris(4),errs(4),vals(4)] = composita(@functionToPass,0,1,i,10^-5);
+        [ris(5),errs(5),vals(5)] = composita(@functionToPass,0,1,i,10^-6);
+        disp(table(tols,ris',errs',vals'));
+    end
+end
+
+
+
 
 function y =  functionToPass(x)
-    y = exp(3.*x);
+    y = sin(1./(0.1+x));
 end
 
 function [If,err,nfeval] = composita(fun,a,b,n,tol)
     err = tol + 1;
     nfeval = 0;
     m = n;
-    ite = 0;
     If = 0;
     while (err > tol)
         m = 2*m;
         h = (b-a)/m;
         i = (0:m);
         xi = a+i*h;
-        if m == 2*n,
+        if m == 2*n % first time
             fi = fun(xi);
+            nfeval = nfeval + m+1;
         else
-            fi = 
+            fi = repelem(fi,2);
+            fi = fi(1:m+1);
+            fi(2:2:m) = fun(xi(2:2:m));
+            nfeval = nfeval + m/2;
         end
-        nfeval = nfeval + m+1;
-
         if mod(n,2) == 0
             mu = 2;
         else
             mu = 1;
         end
-        %ora ci calcoliamo Inm che sarebbe Ikn
+
         w = weights(n);
-        Ieven = 0; %lui bisogna usare quelli pari + lo zero
+        Ieven = 0;
 
         i = 1;
         k = 1;
@@ -58,11 +77,11 @@ function [If,err,nfeval] = composita(fun,a,b,n,tol)
         If = If * (b-a)/(m);
     
         err = abs(Ieven-If)/(2^(n+mu)-1);
-        ite = ite +1;
-        disp("iteraz-----------" + ite);
-        disp("m = " + m);
-        disp("If = " + If);
-        disp("errore " + En);
+%         disp("-----------");
+%         disp("m = " + m);
+%         disp("If = " + If);
+%         disp("errore " + err);
+%         disp("nfeval " + nfeval);
     end
 end
 
