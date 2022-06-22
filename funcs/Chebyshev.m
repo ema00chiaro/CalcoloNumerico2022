03902e25a8f272feadd63bc2ba7cca81c245963d
ema00chiaro <84520939+ema00chiaro@users.noreply.github.com>
2022-06-22 16:02:24 +0200

    dsa

--- funcs/Chebyshev.m
+++ funcs/Chebyshev.m
@@ -1,4 +1,4 @@
-function xc = Chebyshev(a,b,n)
+function xc = chebyshev(a,b,n)
     if b <= a, error("a deve essere minore di b");end
     if n <= 0, error("il numero ascisse richieste" + ...
             " deve essere positivo");end
